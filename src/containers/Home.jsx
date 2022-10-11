8104f8c04fca1de214376c3d1cc8d5f864660dec
ivan cabeza <ivan_cabegarces@hotmail.com>
2022-10-10 22:26:21 -0500

    se agrego react helmet

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,9 +1,13 @@
 import React, { lazy } from 'react'
 import initialState from '../initialState'
+import {Helmet } from "react-helmet"
 const Products = lazy(() => import('../component/products/Products'))
 const Home = () => {
   return (
     <div>
+      <Helmet>
+        <title>Platzi conf Merch - Productos</title>
+      </Helmet>
       <h3>Home</h3>
       <Products products={initialState.products}/>
     </div>
